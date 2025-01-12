f6b4debf17080d8647e8407a564aa9454efc80cc
이승연 <dltmddus9082@naver.com>
2025-01-12 16:03:19 +0900

    [chore] Spring Security 설정 추가

---
+++ src/main/java/com/umc/ttt/global/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.umc.ttt.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final String[] swaggerUrls = {"/swagger-ui/**", "/v3/**"};
+    private final String[] permittedUrls = {"/**"}; // TODO 추후 수정
+    private final String[] allowedUrls = Stream.concat(Arrays.stream(swaggerUrls), Arrays.stream(permittedUrls))
+            .toArray(String[]::new);
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors
+                        .configurationSource(CorsConfig.corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(allowedUrls).permitAll() // 허용된 경로만 접근 허용
+                        .anyRequest().authenticated()   // 그 외 모든 요청에 대해 인증 요구
+                );
+
+        return http.build();
+    }
+
+}
