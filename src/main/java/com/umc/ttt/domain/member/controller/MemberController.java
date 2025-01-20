4df070a0096155a2ec8e715bfeea5f2d4d07f573
JoEunHyang <click2jeh@naver.com>
2025-01-20 13:39:12 +0900

    [feat #7] 구글 로그인

--- src/main/java/com/umc/ttt/domain/member/controller/MemberController.java
+++ src/main/java/com/umc/ttt/domain/member/controller/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberCommandService memberCommandService;
 
-    @PostMapping("api/sign-up")
+    @PostMapping("/api/sign-up")
     @Operation(summary = "회원가입", description = "서버 테스트용 api입니다. 연동x")
     public ApiResponse<String> signUp(@RequestBody MemberSignUpDTO memberSignUpDTO) throws Exception {
         memberCommandService.signUp(memberSignUpDTO);
@@ -20,6 +20,9 @@ public class MemberController {
 
     }
 
+    /*요청 URI("/jwt-test")는 SecurityConfig 인증 URI 설정부분에서 설정한
+인증 없이 접근 가능한 URI가 아니기 때문에
+AccessToken을 헤더에 담아 보내서 인증을 통과해야만 접근이 가능합니다.*/
     //JWT 서비스 테스트를 위한 API
     @GetMapping("/jwt-test")
     @Operation(summary = "jwtTest 요청", description = "서버 테스트용 api입니다. 연동x")
